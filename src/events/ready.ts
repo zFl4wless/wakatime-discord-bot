0c5942a72156d82646f2b7b58e8784fc6d6cc2f3
zfl4wless <noah.bauditz@live.de>
2023-02-10 22:50:27 +0100

    feat: finishes prisma utils

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,11 @@
+import { prismaClient } from '../db/prisma';
 import { Event } from '../structure/Event';
 
 /**
  * This event is emitted when the bot is ready and used as an example for creating events.
  */
 export default new Event('ready', () => {
+    prismaClient.$connect();
+
     console.log('Ready!');
 });
