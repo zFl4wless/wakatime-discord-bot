55446fa0e394fa7f5e7fc2dba58ccc527fc59ced
zfl4wless <noah.bauditz@live.de>
2023-02-16 13:02:12 +0100

    feat: formats data and improves data embed

--- src/commands/all-time-since-today.ts
+++ src/commands/all-time-since-today.ts
@@ -1,5 +1,5 @@
 import { Command } from '../structure/Command';
-import { request } from '../wakatime/wakatime';
+import { getEmbedFromData, request } from '../wakatime/wakatime';
 
 /**
  * This command gets the total time logged since account created.
@@ -8,14 +8,54 @@ import { request } from '../wakatime/wakatime';
  */
 export default new Command({
     name: 'all-time-since-today',
-    description: 'Gets the total time logged since account created.',
+    description: 'Gets the total time logged since account is created.',
     run: async ({ interaction }) => {
         const response = await request('GET', 'users/current/all_time_since_today', interaction.user.id, interaction);
         if (!response) return;
 
-        await interaction.reply({
-            content: `You have logged ${response.data.data.text} since account creation.`,
-            ephemeral: true,
+        const data = formatResponse(response?.data.data as AllTimeSinceTodayResponse);
+        const dataEmbed = getEmbedFromData(
+            'All Time Since Today',
+            'Gets the total time logged since account is created.',
+            data,
+        );
+
+        await interaction.editReply({
+            embeds: [dataEmbed],
         });
     },
 });
+
+type AllTimeSinceTodayResponse = {
+    decimal: string;
+    digital: string;
+    is_up_to_date: boolean;
+    percent_calculated: number;
+    range: Range;
+    text: string;
+    timeout: number;
+    total_seconds: number;
+};
+type Range = {
+    end: string;
+    end_date: string;
+    end_text: string;
+    start: string;
+    start_date: string;
+    start_text: string;
+    timezone: string;
+};
+function formatResponse(response: AllTimeSinceTodayResponse) {
+    return {
+        decimal: response.decimal,
+        digital: response.digital,
+        'is up to date': response.is_up_to_date,
+        'percent calculated': response.percent_calculated,
+        start: response.range.start_text,
+        end: response.range.end_text,
+        timezone: response.range.timezone,
+        text: response.text,
+        timeout: response.timeout,
+        'total seconds': response.total_seconds,
+    };
+}
--- src/utils/embeds.ts
+++ src/utils/embeds.ts
@@ -1,4 +1,10 @@
 import { EmbedBuilder } from 'discord.js';
 
+export const loadingEmbed = () =>
+    new EmbedBuilder()
+        .setTitle('<a:loading:1075747071644151868> Loading...')
+        .setDescription('Please wait while we fetch the data from the WakaTime API.')
+        .setColor('#FFFFFF');
+
 export const errorEmbed = (title: string, description: string) =>
     new EmbedBuilder().setTitle(title).setDescription(description).setColor('#E03B24');
