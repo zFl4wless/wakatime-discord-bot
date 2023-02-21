6c53d3b2338e17f7c079bdb07396ee403cf5e186
zfl4wless <noah.bauditz@live.de>
2023-02-21 22:19:16 +0100

    feat: adds help command

---
+++ src/commands/help.ts
@@ -0,0 +1,37 @@
+import { EmbedBuilder } from '@discordjs/builders';
+import { Command } from '../structure/Command';
+import { defaultEmbed } from '../utils/embeds';
+
+/**
+ * This command gets information about a single user.
+ *
+ * @see https://wakatime.com/developers#all_time_since_today
+ */
+export default new Command({
+    name: 'help',
+    description: 'A little help center ;)',
+    run: async ({ interaction }) => {
+        const embed = defaultEmbed()
+            .setTitle('Help Center')
+            .setDescription('Here is some useful information about the bot.')
+            .addFields([
+                {
+                    name: 'Developer',
+                    value: '> The developer of this bot is [**Fl4wless#0745**](https://fl4wless.de).',
+                },
+                {
+                    name: 'Source Code',
+                    value: '> The source code of this bot is available on [GitHub](https://github.com/zFl4wless/wakatime-discord-bot). Feel free to contribute!',
+                },
+                {
+                    name: 'Support',
+                    value: '> If you have any questions or suggestions, feel free to direct message me on Discord or open an issue on GitHub.',
+                },
+            ]);
+
+        await interaction.reply({
+            embeds: [embed],
+            ephemeral: true,
+        });
+    },
+});
